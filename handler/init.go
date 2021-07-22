237d8be1076e22a89e01274fc974a41e6e5e1f2e
hirata-luida <k.subac02@gmail.com>
2021-07-23 07:26:28 +0900

    Fix checking logic if migration table exists

--- handler/init.go
+++ handler/init.go
@@ -43,9 +43,13 @@ func Init(c *cli.Context) error {
 }
 
 func isMigrationTableExists(ctx context.Context, client *spanner.Client) (bool, error) {
-	rows, err := execSql(ctx, client, "SELECT count(*) FROM information_schema.Tables WHERE table_name = 'Migrations';")
+	rows, err := execSql(ctx, client, "SELECT count(*) as cnt FROM information_schema.Tables WHERE table_name = 'Migrations';")
 	if err != nil {
 		return false, err
 	}
-	return len(rows) == 1, nil
+	var cnt int64
+	if err = rows[0].ColumnByName("cnt", &cnt); err != nil {
+		return false, err
+	}
+	return cnt == 1, nil
 }
